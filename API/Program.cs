a5a3cb29573ecf6505fb7cb7aac4fce0a6d22ca8
Rohan Sharma <rohan.s@branchx.in>
2023-06-21 17:37:22 +0530

    Added the Client Side

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
